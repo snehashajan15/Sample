9170315e4a0840d56f6d61fa438d063a1b19ed1b
20040795 <20040795@mydbs.ie>
2025-11-06 19:35:39 +0000

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+# Provider configuration
+provider "aws" {
+  region = "eu-north-1"
+}
+
+# Create SSH key
+resource "aws_key_pair" "deploy_key" {
+  key_name   = "automation-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+# Security group for SSH and HTTP
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 instance (Ubuntu 22.04)
+resource "aws_instance" "web" {
+  ami           = "ami-0fa91bc90632c73c9" # Change if not in us-east-1
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deploy_key.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "AutomationServer"
+  }
+}
+
+# Output public IP
+output "public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
