a8bbc1072e44c6bdc1df6fac5767f74ede8d705f
20040795 <20040795@mydbs.ie>
2025-11-08 02:30:56 +0000

    added inventory.ini

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,19 +3,19 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-# Create SSH key
+# Create SSH key pair in AWS using your local public key
 resource "aws_key_pair" "deploy_key" {
-  key_name   = "my-deploy-key"
-  public_key = file("keys/terraform_key.pub")
+  key_name   = "terraform_key"
+  public_key = file("${path.module}/keys/terraform_key.pub")
 }
 
-
-# Security group for SSH and HTTP
+# Security group allowing SSH (22) and HTTP (80)
 resource "aws_security_group" "web_sg" {
   name        = "web-sg"
-  description = "Allow SSH and HTTP"
+  description = "Allow SSH and HTTP access"
 
   ingress {
+    description = "Allow SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -23,6 +23,7 @@ resource "aws_security_group" "web_sg" {
   }
 
   ingress {
+    description = "Allow HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -39,15 +40,21 @@ resource "aws_security_group" "web_sg" {
 
 # EC2 instance (Ubuntu 22.04)
 resource "aws_instance" "web" {
-  ami = "ami-01fd6fa49060e89a6"
-  
- # Change if not in us-east-1
+  ami           = "ami-01fd6fa49060e89a6" # Ubuntu 22.04 for eu-north-1
   instance_type = "t3.micro"
   key_name      = aws_key_pair.deploy_key.key_name
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
     Name = "AutomationServer"
+  }
+
+  # Optional: Define connection for provisioners or remote exec
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("${path.module}/keys/terraform_key")
+    host        = self.public_ip
   }
 }
 
